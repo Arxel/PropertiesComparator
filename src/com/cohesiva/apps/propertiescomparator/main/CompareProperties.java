0a97b7aaa53a9278da1b15a6f5aa19b4d5df7490
Bercikov <hgrabowski@wp.pl>
2013-09-19 16:22:56 +0200

    data output etc..

--- src/com/cohesiva/apps/propertiescomparator/main/CompareProperties.java
+++ src/com/cohesiva/apps/propertiescomparator/main/CompareProperties.java
@@ -5,17 +5,14 @@ import java.util.List;
 
 public class CompareProperties {
 
-	private List<Data> patternListOfDatas = new ArrayList<Data>();
-	private List<Data> toCompareListOfDatas = new ArrayList<Data>();
 	private List<Data> finalOutputListWithDatas = new ArrayList<Data>();
 
-	public CompareProperties(List<Data> patternListOfDatas,
-			List<Data> toCompareListOfDatas) {
-		this.patternListOfDatas = patternListOfDatas;
-		this.toCompareListOfDatas = toCompareListOfDatas;
+	public CompareProperties() {
+		// TODO Auto-generated constructor stub
 	}
 
-	public List<Data> matchProperties() {
+	public List<Data> matchProperties(List<Data> patternListOfDatas,
+			List<Data> toCompareListOfDatas) {
 		for (int i = 0; i < patternListOfDatas.size(); i++) {
 			for (int j = 0; j < toCompareListOfDatas.size(); j++) {
 				if (patternListOfDatas.get(i).equals(
--- src/com/cohesiva/apps/propertiescomparator/main/Main.java
+++ src/com/cohesiva/apps/propertiescomparator/main/Main.java
@@ -12,14 +12,24 @@ public class Main {
 	//	CreateDataFromString sdfs = new CreateDataFromString();
 
 		try {
-			referenceSeparetedData = CreateDataFromString.createListOfData(dii.loadDataToList("file"));
-			otherSeparetedData = CreateDataFromString.createListOfData(dii.loadDataToList("file"));
+			referenceSeparetedData = CreateDataFromString.createListOfData(dii.loadDataToList("messages"));
+			otherSeparetedData = CreateDataFromString.createListOfData(dii.loadDataToList("messages_en_US"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
+		Sort sort = new Sort();
+		sort.SortData(referenceSeparetedData);
+		sort.SortData(otherSeparetedData);
 		
-
+		List<Data> finalOutputListWithDatas = new ArrayList<Data>();
+		
+		CompareProperties cP = new CompareProperties();
+		finalOutputListWithDatas = cP.matchProperties(referenceSeparetedData, otherSeparetedData);
+		
+		DataOutput dO = new DataOutput();
+		dO.displayDataOnScreen(finalOutputListWithDatas);
+		
 	}
 
 }
