f5a799ffc6175226870c8204d00872be650b9cb1
Arxel <tomek.jamka@gmail.com>
2013-09-19 16:24:07 +0200

    ??

--- src/com/cohesiva/apps/propertiescomparator/main/Main.java
+++ src/com/cohesiva/apps/propertiescomparator/main/Main.java
@@ -6,13 +6,13 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) {
-		List<Data> referenceSeparetedData = new ArrayList<Data>();
+		List<Data> patternSeparetedData = new ArrayList<Data>();
 		List<Data> otherSeparetedData = new ArrayList<Data>();
 		DataInputImpl dii = new DataInputImpl();
 	//	CreateDataFromString sdfs = new CreateDataFromString();
 
 		try {
-			referenceSeparetedData = CreateDataFromString.createListOfData(dii.loadDataToList("file"));
+			patternSeparetedData = CreateDataFromString.createListOfData(dii.loadDataToList("file"));
 			otherSeparetedData = CreateDataFromString.createListOfData(dii.loadDataToList("file"));
 		} catch (Exception e) {
 			e.printStackTrace();
