dc4d280f05bee8fd0dfe8c52cd99ec2981d51fb0
Arxel <tomek.jamka@gmail.com>
2013-09-20 10:24:50 +0200

    .

--- src/com/cohesiva/apps/propertiescomparator/main/CompareProperties.java
+++ src/com/cohesiva/apps/propertiescomparator/main/CompareProperties.java
@@ -17,12 +17,12 @@ public class CompareProperties {
 		for (int i = 0; i < patternListOfDatas.size(); i++) {
 			exist = false;
 			for (int j = 0; j < toCompareListOfDatas.size(); j++) {
-				if (patternListOfDatas.get(i).equals(
+				if (equals(patternListOfDatas.get(i),
 						toCompareListOfDatas.get(j))) {
 					exist = true;
 				}
-				//to nie mozesz robic equals bo to sa rozne obiekty !
-				//tylko maja takie same dane w œrodku....
+				// to nie mozesz robic equals bo to sa rozne obiekty !
+				// tylko maja takie same dane w œrodku....
 			}
 			if (!exist) {
 				finalOutputListWithDatas.add(patternListOfDatas.get(i));
@@ -30,6 +30,36 @@ public class CompareProperties {
 		}
 
 		return finalOutputListWithDatas;
+
+	}
+
+	public boolean equals(Data o1, Data o2) {
+
+		if (o1.firstField.compareTo(o2.firstField) > 0) {
+			return false;
+		} else if (o1.firstField.compareTo(o2.firstField) < 0) {
+			return false;
+		} else if (o1.firstField.compareTo(o2.firstField) == 0) {
+			if (o1.secondField.compareTo(o2.secondField) > 0)
+				return false;
+			else if (o1.secondField.compareTo(o2.secondField) < 0)
+				return false;
+			else if (o1.secondField.compareTo(o2.secondField) == 0) {
+				if (o1.thirdField.compareTo(o2.thirdField) > 0)
+					return false;
+				else if (o1.thirdField.compareTo(o2.thirdField) < 0)
+					return false;
+				else if (o1.thirdField.compareTo(o2.thirdField) == 0) {
+					if (o1.thirdField.compareTo(o2.fourthField) > 0)
+						return false;
+					else if (o1.thirdField.compareTo(o2.fourthField) < 0)
+						return false;
+					else if (o1.thirdField.compareTo(o2.fourthField) == 0)
+						return true;
+				}
+			}
+		}
+		return false;
 
 	}
 
